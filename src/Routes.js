410f75aa8348e89c7c2eb65b71b71ef53f4962f7
infodaddy <github.com@infodaddy.com>
2020-10-07 14:47:46 -0700

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,4 +1,8 @@
+import React from 'react'
 import ResetPassword from "./containers/ResetPassword";
+import Amplify from 'aws-amplify'
+import config from '../aws-exports'
+Amplify.configure(config)
 
 <UnauthenticatedRoute exact path="/login/reset">
   <ResetPassword />
