193267de09792fff08d2e2ac71a7e2a5aa245c39
infodaddy <github.com@infodaddy.com>
2020-10-09 19:05:36 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,15 +26,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
-    {
-      resolve: `gatsby-theme-amplify-cognito`,
-      options: {
-          region: '',
-          userPoolId: '',
-          userPoolWebClientId: ''
-      },
-  }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
